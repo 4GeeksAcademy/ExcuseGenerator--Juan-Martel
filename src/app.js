6e9ca8c40bd7efdc8e2b4df190572a4edb0669c4
jjmartel <juanomartel@gmail.com>
2025-03-19 20:05:22 +0000

    excuseFinalized

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const excuseFunction = () => {
+    const subject = ['a dog', 'a random kid', 'a policeman', 'my mom', 'the boss'];
+    const action = ['pooped in', 'stole', 'shot at', 'took away', 'destroyed'];
+    const target = ['my jacket', 'my car', 'my window', 'my food', 'my laptop'];
+    const when = [ 'while I was flying.', 'after dancing.', 'when I stopped.', 'drinking mate.'];
+
+    const randomSubject = subject[Math.floor(Math.random() * subject.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomTarget = target[Math.floor(Math.random() * target.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    const excuse = `I wanted to go but ${randomSubject} ${randomAction} ${randomTarget} ${randomWhen}`;
+    document.getElementById('excuse').innerText = excuse;
+
+  };
+
+excuseFunction()
+
 };
