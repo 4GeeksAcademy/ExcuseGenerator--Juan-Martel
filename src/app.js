b3eebcb09bc6d1325c9c2ac31e4eb3b5212246dc
jjmartel <juanomartel@gmail.com>
2025-03-25 18:41:42 +0000

    cambios realizados

--- src/app.js
+++ src/app.js
@@ -6,17 +6,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
   const excuseFunction = () => {
-    const subject = ['a dog', 'a random kid', 'a policeman', 'my mom', 'the boss'];
+    const subject = ['A dog', 'A random kid', 'A policeman', 'My mom', 'The boss'];
     const action = ['pooped in', 'stole', 'shot at', 'took away', 'destroyed'];
     const target = ['my jacket', 'my car', 'my window', 'my food', 'my laptop'];
     const when = [ 'while I was flying.', 'after dancing.', 'when I stopped.', 'drinking mate.'];
 
-    const randomSubject = subject[Math.floor(Math.random() * subject.length)];
-    const randomAction = action[Math.floor(Math.random() * action.length)];
-    const randomTarget = target[Math.floor(Math.random() * target.length)];
-    const randomWhen = when[Math.floor(Math.random() * when.length)];
-
-    const excuse = `I wanted to go but ${randomSubject} ${randomAction} ${randomTarget} ${randomWhen}`;
+    const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+    const excuse = `${getRandomElement(subject)} ${getRandomElement(action)} ${getRandomElement(target)} ${getRandomElement(when)}`;
+      
     document.getElementById('excuse').innerText = excuse;
 
   };
